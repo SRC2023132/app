3d905ccc40d6b0e1236d24e13912c7fa18b7fdd5
SRC2023132 <rrjayaram2003@gmail.com>
2023-11-02 15:55:37 +0530

    application

---
+++ Main/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'; 
+
+const About = () => {
+  return (
+    <div className="about-us-container">
+      <div className="white-box">
+        <h2>About Us</h2>
+        <p>
+        Every year, all our institutions maintain the set trend by achieving 100% pass results, and in the process secure overall State Ranks, Subject State Rank marks, etc. Our performance in academics is indeed praiseworthy, yet we do not tend to overlook co-curricular activities as well. Most of us are aware of the vital fact that of late, students are expected to fully equip themselves such that they are able to face the rigors of life at the global level. No doubt, the Trust spares no efforts, whatsoever, in this particular direction.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
